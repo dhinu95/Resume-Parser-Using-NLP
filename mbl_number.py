9967d2e57b8d4b71316ae0949c58dd1625b47e9a
Dinesh <50333164+dhinu95@users.noreply.github.com>
2019-09-27 10:29:47 +0530

    Create mbl_number.py

---
+++ mbl_number.py
@@ -0,0 +1,10 @@
+def extract_mobile_number(resume_text):
+    phone = re.findall(re.compile(r'(?:(?:\+?([1-9]|[0-9][0-9]|[0-9][0-9][0-9])\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([0-9][1-9]|[0-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?'), resume_text)
+    
+    if phone:
+        number = ''.join(phone[0])
+        if len(number) > 10:
+            return number
+        else:
+            return number
+print('Mobile Number: ',extract_mobile_number(textinput))
