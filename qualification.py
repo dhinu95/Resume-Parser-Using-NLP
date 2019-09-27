bd4d13941145ba8d0ccf0b802d56edf2d7ffb62f
Dinesh <50333164+dhinu95@users.noreply.github.com>
2019-09-27 10:32:25 +0530

    Create qualification.py

---
+++ qualification.py
@@ -0,0 +1,42 @@
+import re
+from nltk.corpus import stopwords
+
+
+# Grad all general stop words
+STOPWORDS = set(stopwords.words('english'))
+
+# Education Degrees
+EDUCATION = [
+            'BE','B.E.', 'B.E', 'BS', 'B.S', 
+            'ME', 'M.E', 'M.E.', 'M.B.A', 'MBA', 'MS', 'M.S', 
+            'BTECH', 'B.TECH', 'M.TECH', 'MTECH', 
+            'SSLC', 'SSC' 'HSC', 'CBSE', 'ICSE', 'X', 'XII'
+        ]
+
+def extract_education(resume_text):
+    nlp_text = nlp(resume_text)
+
+    # Sentence Tokenizer
+    nlp_text = [sent.string.strip() for sent in nlp_text.sents]
+
+    edu = {}
+    # Extract education degree
+    for index, text in enumerate(nlp_text):
+        for tex in text.split():
+            # Replace all special symbols
+            tex = re.sub(r'[?|$|.|!|,]', r'', tex)
+            if tex.upper() in EDUCATION and tex not in STOPWORDS:
+                edu[tex] = text + nlp_text[index + 1]
+                
+                
+
+    # Extract year
+    education = []
+    for key in edu.keys():
+        year = re.search(re.compile(r'(((20|19)(\d{})))'), edu[key])
+        if year:
+            education.append((key, ''.join(year[0])))
+        else:
+            education.append(key)
+    return education
+print('Qualification: ',extract_education(textinput))
